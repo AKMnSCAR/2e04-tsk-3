69802d6bf9eb9b1f42ac7e3310bf245dfa6a6040
AKMnSCAR <115064941+AKMnSCAR@users.noreply.github.com>
2022-10-19 14:43:45 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,30 @@
+let y = 0
 let xx = 0
 input.onButtonPressed(Button.A, function () {
-    xx = 0
-    while (xx <= 5) {
-        led.plot(xx, 0)
-        basic.pause(100)
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
         basic.clearScreen()
-        xx += 1
+        led.plot(0, y)
+        basic.pause(100)
+        y += 1
+    }
+    if (y > 4) {
+        y = 0
     }
 })
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
+        led.plot(xx, 0)
+        basic.pause(100)
+        xx += 1
+    }
+    if (xx > 4) {
+        xx = 0
+    }
 })
